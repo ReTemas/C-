57336fb48531c36854639b333dc6e6d29dc60543
ReTemas <40425134+ReTemas@users.noreply.github.com>
2018-08-30 19:02:26 +0600

    ClassHomeWork
    Add 30.08.2018

---
+++ Class_HW/Money.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_HW
+{
+    public class Money
+    {
+        private int _denomination;
+        private int _countOfNote;
+
+        public Money(int denomination, int countOfNote)
+        {
+            SetDenomination(denomination);
+            SetCountOfNote(countOfNote);
+        }
+
+        public void SetDenomination(int denomination)
+        {
+            _denomination = denomination;
+        }
+
+        public int GetDenomination() {
+            return _denomination;
+        }
+
+        public void SetCountOfNote(int countOfNote) {
+            _countOfNote = countOfNote;
+        }
+
+        public int GetCountOfNote() {
+            return _countOfNote;
+        }
+
+        public int GetSumm()
+        {
+            return _denomination * _countOfNote;
+        }
+
+        public bool CanIBuy(int priceOfGoods)
+        {
+            if(priceOfGoods <= _denomination * _countOfNote)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public int HowMuchCanIBuy(int priceOfGoods)
+        {
+            int countOfGoods = 0;
+            int summOfMoney = _denomination * _countOfNote;
+
+            while(priceOfGoods <= summOfMoney)
+            {
+                summOfMoney -= priceOfGoods;
+                countOfGoods++;
+            }
+            return countOfGoods;
+        }
+    }
+}
+ 
---
+++ Class_HW/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int denomination;
+            int countOfNote;
+            try
+            {
+                Console.WriteLine("Введите номинал купюры.");
+                denomination = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите кол-во купюр.");
+                countOfNote = Convert.ToInt32(Console.ReadLine());
+
+                Money money = new Money(denomination, countOfNote);
+
+                Console.WriteLine("Содержание кошелька.");
+                Console.WriteLine("Номинал: " + money.GetDenomination());
+                Console.WriteLine("Кол-во: " + money.GetCountOfNote());
+                Console.WriteLine("Общая сумма: " + money.GetSumm());
+
+
+
+
+
+                Console.WriteLine("\nПроверка на возможность приобретения товара по заданой цене.\nВведите цену товара.");
+                if (money.CanIBuy(Convert.ToInt32(Console.ReadLine())))
+                {
+                    Console.WriteLine("У вас хватает денег");
+                }
+                else Console.WriteLine("У вас не хватает денег!");
+
+                Console.WriteLine("\nПодсчет кол-во товара которую есть возможность приобрести по заданой цене.\nВведите цену товара.");
+                Console.WriteLine("Кол-во: " + money.HowMuchCanIBuy(Convert.ToInt32(Console.ReadLine())));
+
+            }
+            catch
+            {
+                Console.WriteLine("Следующий раз введите число\nДосвидания!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
