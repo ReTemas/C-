28aa9bcbdeedc4032f72e05dd15df49c26b91b22
ReTemas <40425134+ReTemas@users.noreply.github.com>
2018-09-02 17:33:56 +0600

    23.08.2018

---
+++ PracWork1/PracWork1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracWork1 {
+    class Program {
+        static void Main(string[] args) {
+            int SumOfNumbers = 0;
+            int counter = 0;
+
+            Console.WriteLine("Темирлан");
+            Console.WriteLine("Сулейменов");
+
+            try {
+                while (counter++ < 10)
+                {
+                    SumOfNumbers += Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("Сумма чисел: " + SumOfNumbers);
+            }
+
+            catch {
+                Console.WriteLine("Ошибка преобразования");
+            }
+            Console.ReadLine();
+        }
+    }
+}
