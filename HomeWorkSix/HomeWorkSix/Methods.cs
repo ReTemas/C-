fd677181eb7d889f4177e2a9201193788aabffe5
ReTemas <40425134+ReTemas@users.noreply.github.com>
2018-09-06 17:55:31 +0600

    06.09.2018

---
+++ HomeWorkSix/HomeWorkSix/Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace HomeWorkSix
+{
+    public class Methods
+    {
+        public void Du()
+        {
+            Console.WriteLine("Du!");
+            DuHast();
+        }
+
+        public void DuHast()
+        {
+            Console.WriteLine("DuHast!");
+            Mich();
+        }
+
+        public void Mich()
+        {
+            Console.WriteLine("Mich!");
+            throw new IOException();
+            //IOException ioexception = new IOException("О привет, а я тебя знаю!");
+            //throw ioexception.Message;
+           // return /*ioexception.Message;*/ "О привет";
+        }
+    }
+}
---
+++ HomeWorkSix/HomeWorkSix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using static System.Console;
+
+namespace HomeWorkSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods rammstein = new Methods();
+            try
+            {
+                rammstein.Du();
+            }
+            catch(IOException iosexceptions)
+            {
+                WriteLine(iosexceptions.Message);
+            }
+
+            ReadLine();
+        }
+    }
+}
